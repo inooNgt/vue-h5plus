46817eb6cb196c981c319d7fc0fdabf821a3e9bf
ngt <inoongt@gmail.com>
2018-06-05 19:41:30 +0800

    init

---
+++ src/pages/my/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import "mint-ui/lib/style.css";
+
+export default {
+  name: "App"
+};
+</script>
+
+
+
+<style>
+</style>
---
+++ src/pages/my/components/Index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <mt-tabbar v-model="selected">
+      <mt-tab-item id="外卖">
+        <img slot="icon"> 外卖
+      </mt-tab-item>
+      <mt-tab-item id="订单">
+        <img slot="icon"> 订单
+      </mt-tab-item>
+      <mt-tab-item id="发现">
+        <img slot="icon"> 发现
+      </mt-tab-item>
+      <mt-tab-item id="我的">
+        <img slot="icon"> 我的
+      </mt-tab-item>
+    </mt-tabbar>
+
+    <mt-tab-container v-model="active">
+      <mt-tab-container-item id="tab-container1">
+        1111111
+      </mt-tab-container-item>
+      <mt-tab-container-item id="tab-container2">
+        22222
+      </mt-tab-container-item>
+      <mt-tab-container-item id="tab-container3">
+        333333
+      </mt-tab-container-item>
+    </mt-tab-container>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Tabbar, TabItem, TabContainer, TabContainerItem } from "mint-ui";
+
+Vue.component(TabContainer.name, TabContainer);
+Vue.component(TabContainerItem.name, TabContainerItem);
+
+Vue.component(Tabbar.name, Tabbar);
+Vue.component(TabItem.name, TabItem);
+
+export default {
+  name: "HelloWorld",
+  props: ["selected", "active"],
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
